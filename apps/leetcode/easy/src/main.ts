9a685332fd5a3b3961ed758f1f7321898a9ed93e
lloydlobo <lloyd@lloydlobo.com>
2022-06-12 20:40:18 +0530

    test(easy): trim-mean - it works

--- apps/leetcode/easy/src/main.ts
+++ apps/leetcode/easy/src/main.ts
@@ -11,19 +11,17 @@ function main() {
   // findNearestValidPoint();
   // console.log('twoSum', twoSum([2, 7, 11, 15], 9)); // [0, 1] console.log('twoSum', twoSum([3, 2, 4], 6)); // [1, 2] console.log('twoSum', twoSum([3, 3], 6)); // [0, 1] console.log('twoSum', twoSum([3, 2, 4, 8, 12, 32], 20)); // [3, 4] console.log('twoSum', twoSum([3, 2, 4, 8, 12, 32], 34)); // [1, 5]
   // console.log('twoSumRecursive', twoSumRecursive([2, 7, 11, 15], 9)); // [0, 1]
-  const heads: ListNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))); // prettier-ignore
-  const removedLinkElem = removeElements(heads, 6);
-  console.log(removedLinkElem);
-  console.log(trimMean([1, 2, 3, 4, 5]));
-  // console.clear();
-  console.log(`Gloria In Excelsis Deo!`);
+  // const heads: ListNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))); // prettier-ignore
+  // const removedLinkElem = removeElements(heads, 6);
+  // console.log(removedLinkElem);
+  // console.log( trimMean([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ])); // 10.5
 }
 
 main();
 
 // endregion:   --- main ---
 
-const head = {
+/* const head = {
   val: 1,
   next: {
     val: 2,
@@ -43,4 +41,4 @@ const head = {
       },
     },
   },
-};
+}; */
